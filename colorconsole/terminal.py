25d43d6e312ceb1105f7b4e2c5052d3215704570
Alexander Lavrukov <alexander.lavrukov@gmail.com>
2014-10-10 00:23:35 +0600

    collection formating correction

--- colorconsole/terminal.py
+++ colorconsole/terminal.py
@@ -29,22 +29,24 @@ from __future__ import print_function
 import os
 import sys
 
-colors = { "BLACK"   : 0,
-          "BLUE"    : 1,
-          "GREEN"   : 2,
-          "CYAN"    : 3,
-          "RED"     : 4,
-          "PURPLE"  : 5,
-          "BROWN"   : 6,
-          "LGREY"   : 7,
-          "DGRAY"   : 8,
-          "LBLUE"   : 9,
-          "LGREEN"  : 10,
-          "LCYAN"   : 11,
-          "LRED"    : 12,
-          "LPURPLE" : 13,
-          "YELLOW"  : 14,
-          "WHITE"   : 15  }
+colors = {
+    "BLACK":    0,
+    "BLUE":     1,
+    "GREEN":    2,
+    "CYAN":     3,
+    "RED":      4,
+    "PURPLE":   5,
+    "BROWN":    6,
+    "LGREY":    7,
+    "DGRAY":    8,
+    "LBLUE":    9,
+    "LGREEN":   10,
+    "LCYAN":    11,
+    "LRED":     12,
+    "LPURPLE":  13,
+    "YELLOW":   14,
+    "WHITE":    15,
+}
 
 
 def get_terminal(conEmu=False):
